37658608f01c8d28dd650bdb1e7d901f2460af0c
amanito7 <112196266+amanito7@users.noreply.github.com>
2022-08-27 09:27:18 +0200

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,15 @@
+minetest.register_craftitem("pelmen:pelmen",{
+description = "Pelmen",
+inventory_image = "pelmen.png",
+wield_image = "pelmen.png",
+on_use = minetest.item_eat(4),
+groups = {food_pelmen = 1, fleshy = 3, dig_immediate = 3, flammable = 2}
+})
+minetest.register_craft({
+	output = "pelmen",
+	recipe = {
+		{"","farming:flour",""},
+		{"","extra:ground_meat",""},
+		{"","farming:flour",""}
+	}
+})
